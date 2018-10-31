20555718ea75a55109b36d3f85f05c87760852a9
TheSpeedX <ggspeedx29@gmail.com>
2018-10-31 11:21:55 +0530

    Added Help

--- README.md
+++ README.md
@@ -1,18 +1,77 @@
-SOCKS-Proxy-Checker
-===================
-
+# SOCKER
+This is a SOCKS Proxy Checker
 Checks an IP:Port list of SOCKS4/5 and outputs working proxies to a file.
 
+### HOW To RUN
+To Run type 
+```
+python2 socker.py
+```
+For Help Type
+```
+python2 socker.py help
+```
+Command Line Usage:
+```
+python2 socker.py <socks_file_list> <file_to_write> <threads> <timeout>
+```
 
-Inputs:
+### SOME TERMS:
 
 Proxy list - An absolute path to the file containing a list of proxies in the of IP:Port
 
 Output file - An absolute path to the file that the live SOCKS4/5 proxies will be written to.
 
-Threads - The number of threads that will be used to check proxies. More threads = quicker scanning. If the thread count is too high, your internet connection may be interrupted and false timeouts/connection refused errors will be printed.
+Threads - The number of threads that will be used to check proxies. More threads = quicker 
+scanning. If the thread count is too high, your internet connection may be interrupted and 
+false timeouts/connection refused errors will be printed.
 
-Timeout - The amount of time to give a potential proxy to repond before giving up and trying the next.
+Timeout - The amount of time to give a potential proxy to repond before giving up and trying 
+the next.
 
 
-This script attempts to verify if a given IP:Port listing is a SOCKS4/5 proxy by completeing a SOCKS4/5 handshake with it. In order to maintain the highest level of compatibility I could, I did not use third part libraries and stuck with the default Python libraries. Also, I skipped adding some 'fancy' features, such as a GUI.
+This script attempts to verify if a given IP:Port listing is a SOCKS4/5 proxy by completeing a 
+SOCKS4/5 handshake with it. In order to maintain the highest level of compatibility I could, I 
+did not use third part libraries and stuck with the default Python libraries. Also, I skipped 
+adding some 'fancy' features, such as a GUI.
+
+
+
+# SOCKSLIST
+
+You can Also Use My SOCKS List to get New SOCKS Proxy ...
+
+It Gets Updated Every 24 hours
+
+SOCKER Link : https://github.com/TheSpeedX/SOCKS-List
+
+<a href="https://github.com/TheSpeedX/SOCKS-List">Click Here To Go To SOCK List Page</a>
+
+You Can Also Directly Type this to get SOCKS-List (Make Sure You Have curl installed)
+```
+curl -LO https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks.txt
+```
+
+# NOTES
+
+ NOTE: It is Only For Educational Purposes. Neither I Say Nor I Promote To Do Anything Illegal.
+
+ For Any Queries Mail Me!!!
+ 
+           Mail: ggspeedx29@gmail.com
+           
+  Facebook Page: https://www.facebook.com/GyanaTech
+  
+  To Support Me By Either Helping In Project Or Donating Small Amount To Me For That Contact Me By
+          
+          Mail: ggspeedx29@gmail.com
+          
+ That's All !!!
+
+# CONTACT
+
+ For Any Queries Mail Me!!!
+ 
+           Mail: ggspeedx29@gmail.com
+           
+        Facebook Page: https://www.facebook.com/GyanaTech
--- socker.py
+++ socker.py
@@ -3,8 +3,12 @@ import socket
 import Queue
 import threading
 import time
-
+import os
+import sys
+from random import *
 from struct import *
+
+
 class ThreadChecker(threading.Thread):
 	def __init__(self, queue, timeout):
 		self.timeout = timeout
@@ -77,6 +81,8 @@ class ThreadChecker(threading.Thread):
 				print "Working: " + proxy
 				socksProxies.put(proxy)
 			self.q.task_done()
+
+
 class ThreadWriter(threading.Thread):
 	def __init__(self, queue, outputPath):
 		self.q = queue
@@ -92,47 +98,92 @@ class ThreadWriter(threading.Thread):
 				self.q.task_done()
 			outputFile.close()
 			time.sleep(10)
-checkQueue = Queue.Queue()
-socksProxies = Queue.Queue()
-
-print(' This Script was made by SpeedX')
-if not (len(sys.argv) == 1 or  len(sys.argv) == 3):
-        print "This Script Was Created By SpeedX!!"
-        print "Invalid Parameters used..."
-        print "\n\nUsage:"
-	print "python2 socker.py <socks_file_list> <file_to_write>"
-	print "\n\nBoth parameters are optional...\nBut if used both must be used..."
-	print "Don't use < or > while giving filenames..."
-	print "Remember File Names are case-sensitive...."
-        exit()
-if not (sys.argv[0] == "socker.py" or  sys.argv[0] == "socker"):
-        print "This Script Was Created By SpeedX!!"
-        print "Don't Be OVERSMART by changing script file name or its contents!!"
-        print "Get Your Hands off you chessy ass !!!"
-        exit()
-if len(sys.argv)==3:
-	inputFile=sys.argv[1]
-	outputPath=sys.argv[2]
-inputFile = open(raw_input("Proxy list: "), 'r')
-outputPath = raw_input("Output file: ")
-exists = os.path.isfile(inputFile)
-if not exists:
-	print "The File "+inputFile+" Doesn't exists !!!"
-	print "Try Again !!!"
 
 
-threads = int(raw_input("Number of threads: "))
-timeout = int(raw_input("Timeout(seconds): "))
-for line in inputFile.readlines():
-	checkQueue.put(line.strip('\n'))
-inputFile.close()
-for i in xrange(threads):
-	t = ThreadChecker(checkQueue, timeout)
-	t.setDaemon(True)
-	t.start()
-	time.sleep(.25)
-wT = ThreadWriter(socksProxies, outputPath)
-wT.setDaemon(True)
-wT.start()
-checkQueue.join()
-socksProxies.join()
+
+def info():
+	os.system("clear")
+	os.system("chmod +x .start")
+	os.system("./.start")
+
+def Exit():
+	os.system("clear")
+	numc=randint(30,37)
+	os.system("echo -e \"\\e[1;"+str(numc)+"m\"")
+	os.system("cat .end")
+	os.system("echo -e \"\\e[0m\"")
+	exit()
+
+flag=True
+while flag:
+	info()
+	checkQueue = Queue.Queue()
+	socksProxies = Queue.Queue()
+	if len(sys.argv)==2:
+		print "    Help "
+		print "To run the SOCKER: python2 socker.py"
+		print "1. Give the SOCKS List Path"
+		print "2. Give the FileName To which working Proxy Would be written"
+		print "3. Give Number of threads between 10-15 in phone and 30-50 in PC\n\tNote The Number of threads depends on your processor if u have a high end phone or pc You can use more threads"
+		print "4. Give Timeout between 1-2\n\tNote Faster Your net speed , put your timeout less"
+		print "\n\n Command Line Usage:"
+		print "\t\tpython2 socker.py <socks_file_list> <file_to_write> <threads> <timeout>"
+		print "\nAll parameters are optional...\nBut if used all must be used..."
+		print "Don't use < or > while giving parameters..."
+		print "Remember File Names are case-sensitive...."
+	        print "\n\n\nPress Enter To Continue..."
+		raw_input()
+		Exit()
+	
+	if not (len(sys.argv) == 1 or len(sys.argv) == 5):
+	        print "This Script Was Created By SpeedX!!"
+	        print "Invalid Parameters used..."
+	        print "\n\nUsage:"
+		print "python2 socker.py <socks_file_list> <file_to_write> <threads> <timeout>"
+		print "\n\nAll parameters are optional...\nBut if used all must be used..."
+		print "Don't use < or > while giving parameters..."
+		print "Remember File Names are case-sensitive....\n\nFor More Information Type python2 socker.py help "
+	        print "\n\n\nPress Enter To Continue..."
+		raw_input()
+		Exit()
+	if not (sys.argv[0] == "socker.py" or  sys.argv[0] == "socker"):
+	        print "This Script Was Created By SpeedX!!"
+	        print "Don't Be OVERSMART by changing script file name or its contents!!"
+	        print "Get Your Hands off you chessy ass !!!"
+	        print "\n\n\nPress Enter To Continue..."
+		raw_input()
+		Exit()
+	if len(sys.argv)==5:
+		ifile=sys.argv[1]
+		outputPath=sys.argv[2]
+		threads = int(sys.argv[3])
+		timeout = int(sys.argv[4])
+	else:
+		ifile = raw_input("Proxy list: ")
+		outputPath = raw_input("Output file: ")
+		threads = int(raw_input("Number of threads: "))
+		timeout = int(raw_input("Timeout(seconds): "))
+	exists = os.path.isfile(ifile)
+	if not exists:
+		print "The File "+ifile+" Doesn't exists !!!"
+		print "Try Again !!!"
+		print "Press Enter To Continue..."
+		raw_input()
+		continue
+	else:
+		inputFile=open(ifile,'r')
+	for line in inputFile.readlines():
+		checkQueue.put(line.strip('\n'))
+	print len(line.strip('\n'))+" Proxies Loaded !!!"
+	inputFile.close()
+	for i in xrange(threads):
+		t = ThreadChecker(checkQueue, timeout)
+		t.setDaemon(True)
+		t.start()
+		time.sleep(.25)
+	wT = ThreadWriter(socksProxies, outputPath)
+	wT.setDaemon(True)
+	wT.start()
+	checkQueue.join()
+	socksProxies.join()
+	Exit()
