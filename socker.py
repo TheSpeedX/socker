4b01e8351c997e43e1b1f300caf074d229ae91ce
TheSpeedX <ggspeedx29@gmail.com>
2023-06-06 22:28:08 +0530

    add proxy_tester

--- socker.py
+++ socker.py
@@ -5,6 +5,7 @@ import socket
 import threading
 import time
 import sys
+
 # from random import *
 import struct
 import argparse
@@ -30,7 +31,6 @@ checkQueue = Queue.Queue()
 
 
 class ThreadChecker(threading.Thread):
-
     def __init__(self, queue, timeout):
         self.timeout = timeout
         self.q = queue
@@ -43,7 +43,7 @@ class ThreadChecker(threading.Thread):
         packet4 = b"\x04\x01" + port_pack + ipaddr + b"\x00"
         soc.sendall(packet4)
         data = soc.recv(8)
-        if(len(data) < 2):
+        if len(data) < 2:
             # Null response
             return False
         if data[0] != int("0x00", 16):
@@ -57,7 +57,7 @@ class ThreadChecker(threading.Thread):
     def isSocks5(self, host, port, soc):
         soc.sendall(b"\x05\x01\x00")
         data = soc.recv(2)
-        if(len(data) < 2):
+        if len(data) < 2:
             # Null response
             return False
         if data[0] != int("0x05", 16):
@@ -73,30 +73,30 @@ class ThreadChecker(threading.Thread):
         try:
             port = int(port)
             if port < 0 or port > 65536:
-                print("Invalid: " + proxy)
+                print(f"Invalid: {proxy}")
                 return 0
         except Exception:
-            print("Invalid: " + proxy)
+            print(f"Invalid: {proxy}")
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(self.timeout)
         try:
             s.connect((host, port))
-            if(self.isSocks4(host, port, s)):
+            if self.isSocks4(host, port, s):
                 s.close()
                 return 4
-            elif(self.isSocks5(host, port, s)):
+            elif self.isSocks5(host, port, s):
                 s.close()
                 return 5
             else:
-                print("Not a SOCKS: " + proxy)
+                print(f"Not a SOCKS: {proxy}")
                 s.close()
                 return 0
         except socket.timeout:
-            print("Timeout: " + proxy)
+            print(f"Timeout: {proxy}")
             s.close()
             return 0
         except socket.error:
-            print("Connection refused: " + proxy)
+            print(f"Connection refused: {proxy}")
             s.close()
             return 0
 
@@ -104,8 +104,8 @@ class ThreadChecker(threading.Thread):
         while True:
             proxy = self.q.get()
             version = self.getSocksVersion(proxy)
-            if version == 5 or version == 4:
-                print("Working: " + proxy)
+            if version in [5, 4]:
+                print(f"Working: {proxy}")
                 socksProxies.put(proxy)
             self.q.task_done()
 
@@ -119,18 +119,17 @@ class ThreadWriter(threading.Thread):
     def run(self):
         while True:
             toWrite = self.q.qsize()
-            outputFile = open(self.outputPath, 'a+')
-            for i in range(toWrite):
-                proxy = self.q.get()
-                outputFile.write(proxy + "\n")
-                self.q.task_done()
-            outputFile.close()
+            with open(self.outputPath, "a+") as outputFile:
+                for _ in range(toWrite):
+                    proxy = self.q.get()
+                    outputFile.write(proxy + "\n")
+                    self.q.task_done()
             time.sleep(10)
 
 
 def info():
     print(banner)
-    print("Author: {} \t\t Version: {}".format(__author__, __version__))
+    print(f"Author: {__author__} \t\t Version: {__version__}")
 
 
 def Exit():
@@ -152,30 +151,29 @@ def get_proxies(sources, is_url=False):
                 req = Request(
                     source,
                     headers={
-                        'User-Agent':
-                        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-                    }
+                        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
+                    },
                 )
                 response = urlopen(req)
-                data += response.read().decode('utf-8')+"\n"
+                data += response.read().decode("utf-8") + "\n"
             else:
                 with open(source) as file:
-                    data += file.read()+"\n"
-            print("Processed: " + source)
+                    data += file.read() + "\n"
+            print(f"Processed: {source}")
         except Exception:
-            print("Skipping, Error Occured: " + source)
+            print(f"Skipping, Error Occured: {source}")
     return re.findall(pattern, data)
 
 
 def start_socker(proxies, outputPath, threads, timeout):
-    print("Loaded {} proxies !!!".format(len(proxies)))
+    print(f"Loaded {len(proxies)} proxies !!!")
     for proxy in proxies:
         checkQueue.put(proxy)
-    for i in range(threads):
+    for _ in range(threads):
         thread = ThreadChecker(checkQueue, timeout)
         thread.setDaemon(True)
         thread.start()
-        time.sleep(.25)
+        time.sleep(0.25)
     wT = ThreadWriter(socksProxies, outputPath)
     wT.setDaemon(True)
     wT.start()
@@ -200,24 +198,37 @@ python3 socker.py -i proxys_to_be_checked.txt -auto
 socker is not intented for malicious uses.
 """
 
-parser = argparse.ArgumentParser(description=description,
-                                 epilog='Coded by SpeedX !!!',
-                                 formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument("-o", "--output",
-                    default="live_proxies_{}.txt".format(get_timestamp()),
-                    help="Write Proxies to this file")
-parser.add_argument("-th", "--thread", type=int, default=30,
-                    help="Number of concurrent threads to run")
-parser.add_argument("-t", "--timeout", type=float, default=5.0,
-                    help="Maximum time to wait for response(seconds)")
-parser.add_argument("-i", "--input", action="append",
-                    help="Input File containing proxy")
-parser.add_argument("-auto", "--auto", action="store_true",
-                    help="Fetches proxy from Proxyscrape")
-parser.add_argument("-u", "--url", action="append",
-                    help="Fetch proxy from custom URL")
-parser.add_argument("-v", "--version", action="store_true",
-                    help="show current TBomb version")
+parser = argparse.ArgumentParser(
+    description=description,
+    epilog="Coded by SpeedX !!!",
+    formatter_class=argparse.RawTextHelpFormatter,
+)
+parser.add_argument(
+    "-o",
+    "--output",
+    default=f"live_proxies_{get_timestamp()}.txt",
+    help="Write Proxies to this file",
+)
+parser.add_argument(
+    "-th", "--thread", type=int, default=30, help="Number of concurrent threads to run"
+)
+parser.add_argument(
+    "-t",
+    "--timeout",
+    type=float,
+    default=5.0,
+    help="Maximum time to wait for response(seconds)",
+)
+parser.add_argument(
+    "-i", "--input", action="append", help="Input File containing proxy"
+)
+parser.add_argument(
+    "-auto", "--auto", action="store_true", help="Fetches proxy from Proxyscrape"
+)
+parser.add_argument("-u", "--url", action="append", help="Fetch proxy from custom URL")
+parser.add_argument(
+    "-v", "--version", action="store_true", help="show current TBomb version"
+)
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -229,7 +240,7 @@ if __name__ == "__main__":
         proxies = []
         proxy_api = [
             "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt",
-            "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt"
+            "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt",
         ]
         if args.input:
             proxies.extend(get_proxies(args.input))
