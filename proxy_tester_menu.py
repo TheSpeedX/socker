8569c62d1b58d056cdb2743690f5d84899348d30
Suman <83931935+5h4kun1@users.noreply.github.com>
2023-06-08 22:43:58 +0530

    Update proxy_tester_menu.py
    The `argparse` library is used to handle command-line arguments. It allows specifying the input file, output file, domain, number of threads, and the option to use the `ftreq` library for faster HTTP requests.
    - The `test_proxy` function now uses the
    
     `urllib` library to make HTTP requests with the specified proxy. It builds an opener with the proxy URL and attempts to open the target URL. If an error occurs, it returns `False`, indicating an inactive proxy.
    - The `test_ip_addresses` function and the `main` function are modified to use the command-line arguments passed through `args`.
    - The `check_input_file` function is added to validate the input file's existence before starting the testing.

--- proxy_tester_menu.py
+++ proxy_tester_menu.py
@@ -1,32 +1,38 @@
 import contextlib
 import time
-import requests
 import os
+import argparse
 import concurrent.futures
+import urllib.request
+from urllib.error import URLError, HTTPError
+import ftreq
 
 DEFAULT_DOMAIN = "www.google.com"
 
 
-def test_proxy(ip, domain=DEFAULT_DOMAIN):
-    proxies = {
-        "http": f"http://{ip}",
-        "https": f"http://{ip}",
-    }
-    with contextlib.suppress(requests.RequestException):
-        response = requests.get(f"http://{domain}", proxies=proxies, timeout=5)
-        if response.status_code >= 200 and response.status_code < 300:
-            return True
-    return False
+def test_proxy(ip, domain=DEFAULT_DOMAIN, use_ftreq=False):
+    url = f"http://{domain}"
+    proxy_url = f"http://{ip}"
+    opener = urllib.request.build_opener(urllib.request.ProxyHandler({'http': proxy_url, 'https': proxy_url}))
+
+    try:
+        if use_ftreq:
+            ftreq.get(url, opener=opener)
+        else:
+            urllib.request.urlopen(url, timeout=5, opener=opener)
+        return True
+    except (URLError, HTTPError):
+        return False
 
 
-def test_ip_addresses(ip_file, output_file, domain=DEFAULT_DOMAIN, num_threads=10):
+def test_ip_addresses(ip_file, output_file, domain=DEFAULT_DOMAIN, num_threads=10, use_ftreq=False):
     with open(ip_file, "r") as file:
         ip_addresses = file.read().splitlines()
 
     results = []
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-        future_to_ip = {executor.submit(test_proxy, ip, domain): ip for ip in ip_addresses}
+        future_to_ip = {executor.submit(test_proxy, ip, domain, use_ftreq): ip for ip in ip_addresses}
         for future in concurrent.futures.as_completed(future_to_ip):
             ip = future_to_ip[future]
             try:
@@ -43,80 +49,40 @@ def test_ip_addresses(ip_file, output_file, domain=DEFAULT_DOMAIN, num_threads=1
     print("\rTesting complete.                  ")
 
 
-def display_menu():
-    print("Select a file to test:")
-    print("1. http.txt")
-    print("2. https.txt")
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Proxy Tester")
+    parser.add_argument("input_file", type=str, help="Path to the input file containing IP addresses")
+    parser.add_argument("output_file", type=str, help="Path to the output file to save the results")
+    parser.add_argument("--domain", type=str, default=DEFAULT_DOMAIN,
+                        help="Domain to test the proxies on (default: www.google.com)")
+    parser.add_argument("--threads", type=int, default=10,
+                        help="Number of threads to use for testing (default: 10)")
+    parser.add_argument("--ftreq", action="store_true",
+                        help="Use faster-than-requests library for making requests (optional)")
+
+    return parser.parse_args()
 
 
-def get_user_choice():
-    while True:
-        choice = input("Enter your choice (1 or 2): ")
-        if choice in ["1", "2"]:
-            return choice
-        print("Invalid choice. Please try again.")
-
-
-def get_input_filename(choice):
-    if choice == "1":
-        return "http.txt"
-    elif choice == "2":
-        return "https.txt"
-
-
-def check_input_files():
-    http_file = "http.txt"
-    https_file = "https.txt"
-    if not os.path.exists(http_file) or not os.path.isfile(http_file):
-        print(f"Error: '{http_file}' file is missing or not found.")
-        return False
-    if not os.path.exists(https_file) or not os.path.isfile(https_file):
-        print(f"Error: '{https_file}' file is missing or not found.")
+def check_input_file(input_file):
+    if not os.path.exists(input_file) or not os.path.isfile(input_file):
+        print(f"Error: '{input_file}' file is missing or not found.")
         return False
     return True
 
 
-def get_domain_choice():
-    domain_choice = input(
-        "Enter the domain on which you want to test the proxies (default: www.google.com): "
-    )
-    return domain_choice.strip() or DEFAULT_DOMAIN
-
-
-def get_num_threads():
-    num_threads = input("Enter the number of threads to use for testing (default: 10): ")
-    try:
-        num_threads = int(num_threads)
-        if num_threads <= 0:
-            print("Invalid number of threads. Using the default value.")
-            num_threads = 10
-    except ValueError:
-        print("Invalid input. Using the default number of threads.")
-        num_threads = 10
-    return num_threads
-
-
 def main():
-    if not check_input_files():
-        return
+    args = parse_arguments()
 
-    display_menu()
-    choice = get_user_choice()
-    input_file = get_input_filename(choice)
-    output_file = "results.txt"
+    if not check_input_file(args.input_file):
+        return
 
     print("Testing in progress...")
     time.sleep(1)  # Simulate a delay before starting the testing
 
-    domain = get_domain_choice()
-    print(f"Using domain: {domain}")
+    test_ip_addresses(args.input_file, args.output_file, domain=args.domain,
+                      num_threads=args.threads, use_ftreq=args.ftreq)
 
-    num_threads = get_num_threads()
-    print(f"Using {num_threads} threads for testing.")
-
-    test_ip_addresses(input_file, output_file, domain=domain, num_threads=num_threads)
-
-    print("Testing complete. Results saved to", output_file)
+    print("Testing complete. Results saved to", args.output_file)
 
 
 if __name__ == "__main__":
